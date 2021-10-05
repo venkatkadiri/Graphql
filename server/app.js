11e9c5798444bff646403d26ac6e66f57c8f831d
venkatkadiri <venkatkadiri@hotmail.com>
2021-10-05 23:59:06 +0530

    Root Query complete

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
 const express = require("express");
-var { graphqlHTTP } = require("express-graphql");
-const Schema = require("./schema/schema");
+var { graphqlHTTP } = require("express-graphql"); // CommonJS
+const schema = require("./schema/schema");
 
 const app = express();
+
+// bind express with graphql
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
+    graphiql: true,
   })
 );
 
 app.listen(4000, () => {
-  console.log("listening for request on port 4000");
+  console.log("now listening for requests on port 4000");
 });
